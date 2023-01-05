7197df2299221756508e80bb78c5a45883329868
Kamilla Latypova <latipova.kamilla@gmail.com>
2023-01-05 01:47:30 +0100

    save train results to out file

--- src/train.py
+++ src/train.py
@@ -1,29 +1,45 @@
+import os.path
+from datetime import datetime
+
 import torch
-from torch.distributions import Beta
-from tqdm import tqdm
-from datasets.main import generate_training_datasets
-from torch.utils.data import DataLoader
-from networks.generator import generator_nn
-from networks.discriminator import discriminator_nn
 from torch import nn
+from torch.distributions import Beta
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+from datasets.main import generate_training_datasets
+from networks.discriminator import discriminator_nn
+from networks.generator import generator_nn
 
 device = 'cpu'
 
 
 def disc_loss(disc_pred, ground_truth_val):
-  criterion = nn.MSELoss()
-  ground_truth = torch.ones_like(disc_pred) * ground_truth_val
-  loss = criterion(disc_pred, ground_truth)
-  return loss
+    criterion = nn.MSELoss()
+    ground_truth = torch.ones_like(disc_pred) * ground_truth_val
+    loss = criterion(disc_pred, ground_truth)
+    return loss
+
 
 def gen_loss(gen_point, ground_truth):
-  criterion = nn.MSELoss()
-  ground_truth = ground_truth.view(gen_point.shape)
-  loss = criterion(gen_point, ground_truth)
-  return loss
+    criterion = nn.MSELoss()
+    ground_truth = ground_truth.view(gen_point.shape)
+    loss = criterion(gen_point, ground_truth)
+    return loss
+
+
+def create_file():
+    filename = f"out_{datetime.now().strftime('%Y%m%d-%H%M%S')}.txt"
+    file_src = os.path.join("out", filename)
+    f = open(file_src, 'x')
+    return f
 
 
 def train(epochs, batch_size, lambda_1, lambda_2, alfa, beta, beta_1, beta_2, learning_rate):
+    out_file = create_file()
+    out_file.write(f"Started training file with epochs: {epochs}, batch_size: {batch_size}, "
+                   f"lambda_1: {lambda_1}, lambda_2: {lambda_2}, alfa: {alfa}, beta: {beta}, "
+                   f"beta_1: {beta_1}, beta_2: {beta_2}, learning_rate: {learning_rate}\n")
     discriminator_optimizer = torch.optim.Adam(discriminator_nn.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
     generator_optimizer = torch.optim.Adam(generator_nn.parameters(), lr=learning_rate, betas=(beta_1, beta_2))
     beta_distribution = Beta(torch.tensor([alfa]), torch.tensor([beta]))
@@ -81,9 +97,6 @@ def train(epochs, batch_size, lambda_1, lambda_2, alfa, beta, beta_1, beta_2, le
         avg_disc_loss = total_discriminator_loss / (len(normal_trainloader) * len(outliers_trainloader))
         avg_gen_loss = total_generator_loss / (len(normal_trainloader) * len(outliers_trainloader))
 
-        avg_disc_loss = total_discriminator_loss / (len(normal_trainloader) * len(outliers_trainloader))
-        avg_gen_loss = total_generator_loss / (len(normal_trainloader) * len(outliers_trainloader))
-
         print(f"Epoch: {i + 1} | D: loss {avg_disc_loss} | G: loss {avg_gen_loss}")
-
-
+        out_file.write(f"Epoch: {i + 1} | D: loss {avg_disc_loss} | G: loss {avg_gen_loss}\n")
+    out_file.close()
